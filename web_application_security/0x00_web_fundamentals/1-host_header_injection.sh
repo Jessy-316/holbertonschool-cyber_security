c4196d7c5d735002533fed37e492af94d0e2eb7c
Francisco Baez <jessiebaez316@outlook.com>
2025-05-02 14:30:01 -0400

    task 1 upload

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" -d "$3" | grep -oP '(?<=<a href="http://).*?(?=/login")'
+curl -s -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" -d "$3"
